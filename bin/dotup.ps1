2de8e4c5a658aee3352fdef65b567eabf0a09900
anurse <andrew@andrewnurse.net>
2015-03-02 10:58:12 -0800

    add auto-pull to dotup

--- bin/dotup.ps1
+++ bin/dotup.ps1
@@ -1,1 +1,10 @@
-. $DotFilesRoot\sys\start.ps1
+pushd $DotFilesRoot
+try {
+    git checkout master
+    git pull origin master
+    git submodule update
+
+    . $DotFilesRoot\sys\start.ps1
+} finally {
+    popd
+}
