7fdee416d55d2539f600769278fb95e14056f60c
anurse <andrew@andrewnurse.net>
2015-06-06 10:50:26 -0700

    fix zsh crash from colors

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -1,2 +1,6 @@
 #add each topic folder to fpath so that they can add functions and completion scripts
-for topic_folder ($ZSH/*) if [ -d $topic_folder ]; then  fpath=($topic_folder $fpath); fi;
+for topic_folder ($ZSH/*); do
+    if [ -d $topic_folder ] && [ $(basename $topic_folder) != "vim.symlink" ]; then
+        fpath=($topic_folder $fpath)
+    fi
+done
