440a3b58b7eaebf604f23f26e86a55134418b2cd
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2016-01-08 14:19:46 -0800

    add some docker tools for Windows

--- powershell/docker.profile.ps1
+++ powershell/docker.profile.ps1
@@ -6,3 +6,21 @@ function global:docker-use($MachineName) {
         docker-machine env --shell=powershell $MachineName | Invoke-Expression
     }
 }
+
+<#
+.SYNOPSIS
+    Prints the current working directory, mapped into the docker machine (assuming default settings)
+#>
+function global:docker-pwd {
+    $current = Convert-Path (Get-Location)
+    if(!$current.StartsWith("C:\Users")) {
+        throw "Can't map docker paths unless they are under C:\Users. Sorry :("
+    }
+    $subpath = $current.Substring(3).Replace("\", "/")
+    "/c/$subpath"
+}
+
+function global:dock([string]$Image = "ubuntu") {
+    $dir = docker-pwd
+    docker run -it --rm -v "$($dir):$($dir)" -w $dir $Image
+}
