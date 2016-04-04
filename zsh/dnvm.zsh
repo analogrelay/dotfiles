b79ca2119ad8568d9b0dab6b22cc1dd5a692ebbe
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2016-04-04 16:58:07 -0700

    updates

--- zsh/dnvm.zsh
+++
@@ -1,5 +0,0 @@
-if [ -f "$HOME/Code/aspnet/dnvm/src/dnvm.sh" ]; then
-    source "$HOME/Code/aspnet/dnvm/src/dnvm.sh"
-elif [ -f "$HOME/.dnx/bin/dnvm.sh" ]; then
-    source "$HOME/.dnx/bin/dnvm.sh"
-fi
