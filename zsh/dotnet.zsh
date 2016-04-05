e3b414a42ea4571b4361e5bb4f65b16f01e46b90
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2016-04-04 17:01:32 -0700

    fix PATHs

--- zsh/dotnet.zsh
+++
@@ -1,9 +0,0 @@
-UNAME=$(uname)
-if [ $UNAME = Darwin ]; then
-    export DOTNET_REFERENCE_ASSEMBLIES_PATH=/Library/Frameworks/Mono.framework/Versions/Current/lib/mono/xbuild-frameworks
-else if [ -d "/usr/local/lib/mono/xbuild-frameworks" ]; then
-    export DOTNET_REFERENCE_ASSEMBLIES_PATH=/usr/local/lib/mono/xbuild-frameworks
-else if [ -d "/usr/lib/mono/xbuild-frameworks" ]; then
-    export DOTNET_REFERENCE_ASSEMBLIES_PATH=/usr/lib/mono/xbuild-frameworks
-fi
-
