3145e009440258f2e42ec26406bb28c4bab76ade
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2016-08-12 11:35:28 -0700

    bunch of new wsl stuff

--- zsh/code.zsh
+++ zsh/code.zsh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
 code() {
-    if [[ $# = 0 ]]
-    then
-        open -a "Visual Studio Code"
+    if [ $WSL = "1" ]; then
+        if ! type -p wstart >/dev/null 2>/dev/null; then
+            echo "Can't find 'wstart'. Install cbwin to call back to Windows apps." 1>&2
+            return 1
+        fi
+        wstart "C:\Progra~2\MIFA7F~1\Code.exe" "$@"
+    elif [ $(uname) = "Darwin" ]; then
+        if [[ $# = 0 ]]
+        then
+            open -a "Visual Studio Code"
+        else
+            [[ $1 = /* ]] && F="$1" || F="$PWD/${1#./}"
+            open -a "Visual Studio Code" --args "$F"
+        fi
     else
-        [[ $1 = /* ]] && F="$1" || F="$PWD/${1#./}"
-        open -a "Visual Studio Code" --args "$F"
+        echo "Not yet implemented!" 1>&2
+        return 1
     fi
 }
---
+++ zsh/wsl.zsh
@@ -0,0 +1,7 @@
+if [ $WSL != "1" ]; then
+    # We are sourced, so DON'T use exit
+    return 0
+fi
+
+# Use the Docker for Windows host
+export DOCKER_HOST=tcp://0.0.0.0:2375
