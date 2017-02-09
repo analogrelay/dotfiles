2c3dfc7d94a5a0b139011d185fd3483b7cdafdf3
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-02-09 13:47:19 -0800

    clean up ssh-agent initialization

--- powershell/ssh.profile.ps1
+++ powershell/ssh.profile.ps1
@@ -3,5 +3,14 @@ if (Get-Command ssh-agent -ErrorAction SilentlyContinue) {
     $ssh = (Get-Command ssh.exe).Definition
     $env:GIT_SSH=$ssh
 
-    ssh-add
+    $MainKey = Join-Path $env:USERPROFILE ".ssh\id_rsa"
+
+    # Check if we need to add the main identity to the agent
+    $fingerprint = (ssh-keygen -lf $MainKey).Split(" ")[1]
+
+    if(ssh-add -l | select-string $fingerprint) {
+        Write-Host "$MainKey already registered in SSH agent"
+    } else {
+        ssh-add $MainKey
+    }
 }
