03bd30e0da947cef6fbfbf2c2f79be95985fd0bf
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-03-01 15:19:52 -0800

    add vscode keybindings

--- vscode/vscode.dotfiles.install.ps1
+++ vscode/vscode.dotfiles.install.ps1
@@ -1,6 +1,11 @@
-$Link = Join-Path (Join-Path (Join-Path $env:APPDATA "Code") "User") "settings.json"
-$Target = Join-Path (Join-Path $DotFiles_Root "vscode") "settings.json"
+$CodeUserSettingsRoot = Join-Path (Join-Path $env:APPDATA "Code") "User"
+$CodeDotfilesRoot = Join-Path $DotFiles_Root "vscode"
+
 $Global:DotFiles_LinksToCreate += @(@{
-	"Link"=$Link
-	"Target"=$Target
+	"Link" = Join-Path $CodeUserSettingsRoot "settings.json"
+	"Target" = Join-Path $CodeDotfilesRoot "settings.json"
+})
+$Global:DotFiles_LinksToCreate += @(@{
+	"Link" = Join-Path $CodeUserSettingsRoot "keybindings.json"
+	"Target" = Join-Path $CodeDotfilesRoot "keybindings.json"
 })
