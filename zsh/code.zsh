053fd8ba2cad08cdaca2490bb8821b29eba21bfe
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-03-31 11:34:57 -0700

    fix code func

--- zsh/code.zsh
+++ zsh/code.zsh
@@ -1,13 +1,11 @@
-#!/bin/sh
-
 code() {
-    if [ $WSL = "1" ]; then
+    if [[ "$WSL" = "1" ]]; then
         if ! type -p wstart >/dev/null 2>/dev/null; then
             echo "Can't find 'wstart'. Install cbwin to call back to Windows apps." 1>&2
             return 1
         fi
         wstart "C:\Progra~2\MIFA7F~1\Code.exe" "$@"
-    elif [ $(uname) = "Darwin" ]; then
+    elif [[ "$(uname)" = "Darwin" ]]; then
         if [[ $# = 0 ]]
         then
             open -a "Visual Studio Code"
