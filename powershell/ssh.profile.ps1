e14242957436d9898a3ee9b3b76a2daf239c8253
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-04-04 09:49:22 -0700

    add more ssh keys to agent

--- powershell/ssh.profile.ps1
+++ powershell/ssh.profile.ps1
@@ -1,16 +1,38 @@
+# Find Git Unix Tools and put them on the path
+if (!(Get-Command ssh-agent -ErrorAction SilentlyContinue)) {
+    $ScoopPath = Join-Path $env:USERPROFILE "scoop"
+    if (Test-Path $ScoopPath) {
+        $GitSshBin = "$ScoopPath\apps\git\current\usr\bin"
+        if (Test-Path $GitSshBin) {
+            $env:PATH = "$env:PATH;$GitSshBin"
+        }
+    }
+}
+
+$KeysToAdd = @(
+    "id_rsa",
+    "anurse-docker"
+)
+
+function AddKeyIfNotPresent($KeyFile) {
+    # Check if we need to add the main identity to the agent
+    $fingerprint = (ssh-keygen -lf $KeyFile).Split(" ")[1]
+
+    if(ssh-add -l | select-string $fingerprint -SimpleMatch) {
+        Write-Host "$KeyFile already registered in SSH agent"
+    } else {
+        ssh-add $KeyFile
+    }
+}
+
+
 if (Get-Command ssh-agent -ErrorAction SilentlyContinue) {
     Start-SshAgent
     $ssh = (Get-Command ssh.exe).Definition
     $env:GIT_SSH=$ssh
 
-    $MainKey = Join-Path $env:USERPROFILE ".ssh\id_rsa"
-
-    # Check if we need to add the main identity to the agent
-    $fingerprint = (ssh-keygen -lf $MainKey).Split(" ")[1]
-
-    if(ssh-add -l | select-string $fingerprint) {
-        Write-Host "$MainKey already registered in SSH agent"
-    } else {
-        ssh-add $MainKey
+    $KeysToAdd | ForEach-Object {
+        $KeyFile = Join-Path (Join-Path $env:USERPROFILE ".ssh") $_
+        AddKeyIfNotPresent $KeyFile
     }
 }
