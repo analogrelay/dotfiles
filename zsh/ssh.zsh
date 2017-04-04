7d806b857674962ff9e67f848964a43d6637530a
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-04-04 10:13:05 -0700

    clean up ssh on unix

--- zsh/ssh.zsh
+++ zsh/ssh.zsh
@@ -1,3 +1,19 @@
-eval $(ssh-agent)
+_ssh_add_key()
+{
+    local KEYFILE=$1
 
-ssh-add
+    if [ ! -f $KEYFILE ]; then
+        echo "$KEYFILE does not exist"
+    else
+        local FINGERPRINT=$(ssh-keygen -lf $KEYFILE | cut -d' ' -f 2)
+
+        if ssh-add -l | grep $FINGERPRINT >/dev/null; then
+            echo "$KEYFILE is already in the ssh agent"
+        else
+            ssh-add $KEYFILE
+        fi
+    fi
+}
+
+_ssh_add_key ~/.ssh/id_rsa
+_ssh_add_key ~/.ssh/anurse-docker
