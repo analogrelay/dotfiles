82b899ddd107ece5ea7d9758a3292fd1acdce270
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-04-28 11:11:45 -0700

    add wsl_init

--- zsh/wsl.zsh
+++ zsh/wsl.zsh
@@ -3,11 +3,14 @@ if [[ $WSL != "1" ]]; then
     return 0
 fi
 
-# Use the Docker for Windows host
-export DOCKER_HOST=tcp://0.0.0.0:2375
-
 if [ -z $WSL_FIRSTRUN ]; then
     export WSL_FIRSTRUN=1
 
     cd $HOME
 fi
+
+# Use the Docker for Windows host
+export DOCKER_HOST=tcp://0.0.0.0:2375
+
+CMDLINE=$(</proc/self/cmdline)
+export SHELL=$(type $CMDLINE | cut -d' ' -f 3)
