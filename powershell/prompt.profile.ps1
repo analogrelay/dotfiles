1452802ff050d442310346d38298427acbe771a3
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-05-01 14:25:46 -0700

    give powershell some love

--- powershell/prompt.profile.ps1
+++ powershell/prompt.profile.ps1
@@ -1,33 +1,42 @@
+$WindowsSymbol = [char]0xf17a;
+$LinuxSymbol = [char]0xf17c;
+$AppleSymbol = [char]0xf179;
+$DirSymbol = [char]0xf07b;
+$BranchSymbol = [char]0xe0a0;
+$ArrowSymbol = [char]0xe0b0;
+$PromptSymbol = [char]0x203a;
+
+$OsSymbol = $WindowsSymbol
+
 function global:prompt {
     $realLASTEXITCODE = $LASTEXITCODE
 
-    # Reset color, which can be messed up by Enable-GitColors
-    if($GitPromptSettings.DefaultForegroundColor) {
-        $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
-    }
+    Write-Host -ForegroundColor Black -BackgroundColor Cyan " $OsSymbol $([Environment]::MachineName) " -NoNewLine
+    Write-Host -ForegroundColor Cyan -BackgroundColor Yellow $ArrowSymbol -NoNewLine
 
-    Write-Host $pwd -nonewline -ForegroundColor ([ConsoleColor]::Yellow)
+    Write-Host -ForegroundColor Black -BackgroundColor Yellow " $DirSymbol $(Get-Location) " -NoNewLine
 
-    if(Get-Command dotnet -ErrorAction SilentlyContinue) {
-        try {
-            $dotnetVersion = (dotnet --version).Trim()
-        } catch {
-            $dotnetVersion = "<<error>>"
+    git status -s 2>&1 | Out-Null
+    if ($LASTEXITCODE -eq 0) {
+        $Status = git status --porcelain
+        $Branch = git rev-parse --abbrev-ref HEAD
+        if ([string]::IsNullOrWhiteSpace($Status)) {
+            Write-Host -ForegroundColor Yellow -BackgroundColor Green $ArrowSymbol -NoNewLine
+            Write-Host -BackgroundColor Green " $BranchSymbol $Branch " -NoNewLine
+            Write-Host -ForegroundColor Green -BackgroundColor Black $ArrowSymbol -NoNewLine
+        } else {
+            Write-Host -ForegroundColor Yellow -BackgroundColor Red $ArrowSymbol -NoNewLine
+            Write-Host -BackgroundColor Red " $BranchSymbol $Branch " -NoNewLine
+            Write-Host -ForegroundColor Red -BackgroundColor Black $ArrowSymbol -NoNewLine
         }
     }
-
-    if($dotnetVersion) {
-        Write-Host " (dotnet:$dotnetVersion)" -nonewline -ForegroundColor ([ConsoleColor]::Yellow)
-    }
-
-    if(Get-Command Write-VcsStatus -ErrorAction SilentlyContinue) {
-        Write-VcsStatus
+    else {
+        Write-Host -ForegroundColor Yellow -BackgroundColor Black $ArrowSymbol -NoNewLine
     }
 
     Write-Host
-    Write-Host "ps1 #" -nonewline -ForegroundColor ([ConsoleColor]::Green)
 
     $LASTEXITCODE = $realLASTEXITCODE
     $Host.UI.RawUI.WindowTitle = $pwd
-    return " "
+    return "$PromptSymbol "
 }
