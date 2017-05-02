f4deeb32814c2276f128c52b1dd4776cb6b6fb71
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-05-02 16:06:37 -0700

    mostly update ZSH prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,77 +1,95 @@
 autoload colors && colors
-# cheers, @ehrenmurdick
-# http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
 
-MN_BG=6
-MN_FG=0
-DIR_BG=11
-DIR_FG=0
-GIT_CLEAN_BG=10
-GIT_CLEAN_FG=0
-GIT_DIRTY_BG=9
-GIT_DIRTY_FG=0
+typeset -A symbols
+symbols=(
+    windows "\uf17a"
+    linux "\uf17c"
+    apple "\uf179"
+    folder "\uf07b"
+    branch "\ue725"
+    arrow "\ue0b0"
+    prompt "\u203a"
+    dotnet "\ue70c"
+    clock "\uf017"
+)
 
-DIR_SYMBOL="\ue5ff"
-BRANCH_SYMBOL="\ue0a0"
-ARROW_SYMBOL="\ue0b0"
-
-MACHINE_TYPE_SYMBOL=
-UNAME=$(uname)
-BATTERY_STATUS=
 if [[ "$UNAME" == "Darwin" ]]; then
-    MACHINE_TYPE_SYMBOL="\ue711"
-    BATTERY_STATUS=$(pmset -g batt | sed -n '1!p' | sed "s/^ -InternalBattery-0 .id=[0-9]*.[^0-9]*//g" | cut -d ";" -f 1,2)
-elif [[ "$WSL" == "1" ]]; then
-    MACHINE_TYPE_SYMBOL="\ue70f"
+    symbols[os]=$symbols[apple]
 else
-    MACHINE_TYPE_SYMBOL="\ue712"
+    symbols[os]=$symbols[linux]
 fi
 
-if (( $+commands[git] ))
-then
-  git="$commands[git]"
-else
-  git="/usr/bin/git"
-fi
-
-git_branch() {
-  echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
-}
-
-git_dirty() {
-  if $(! $git status -s &> /dev/null)
-  then
-    echo " %F{$DIR_BG}%k$ARROW_SYMBOL%f"
-  else
-    if [[ $($git status --porcelain) == "" ]]
-    then
-      echo " %F{$DIR_BG}%K{$GIT_CLEAN_BG}$ARROW_SYMBOL %K{$GIT_CLEAN_BG}%F{$GIT_CLEAN_FG}$(git_prompt_info)%k%F{$GIT_CLEAN_BG}$ARROW_SYMBOL%f"
-    else
-      echo " %F{$DIR_BG}%K{$GIT_DIRTY_BG}$ARROW_SYMBOL %K{$GIT_DIRTY_BG}%F{$GIT_DIRTY_FG}$(git_prompt_info)%k%F{$GIT_DIRTY_BG}$ARROW_SYMBOL%f"
+color() {
+    FG_COLOR=
+    if [ ! -z $1 ]; then
+        FG_COLOR="$fg[$1]"
     fi
-  fi
+    BG_COLOR=
+    if [ ! -z $2 ]; then
+        BG_COLOR="$bg[$2]"
+    fi
+    echo -e "${FG_COLOR}${BG_COLOR}"
 }
 
-git_prompt_info () {
- ref=$($git symbolic-ref HEAD 2>/dev/null) || return
-# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
- echo "$BRANCH_SYMBOL ${ref#refs/heads/}"
+color_reset() {
+    echo -e "\e[0m"
 }
 
-directory_name() {
-  echo " $DIR_SYMBOL %K{$DIR_BG}%F{$DIR_FG}%~%\/"
+write_colored() {
+    local MESSAGE=$1
+    local FG=$2
+    local BG=$3
+
+    echo "$(color $FG $BG)${MESSAGE}$(color_reset)"
 }
 
-machine_name() {
-  echo -e "%K{$MN_BG}%F{$MN_FG} $MACHINE_TYPE_SYMBOL $(hostname) %K{$DIR_BG}%F{$MN_BG}\ue0b0"
+SEGMENT_FG=black
+SEGMENT_BG=white
+
+write_segment() {
+    local CONTENT=$1
+    echo -n "$(color $SEGMENT_FG $SEGMENT_BG) ${CONTENT} $(color_reset)"
 }
 
-export PROMPT=$'\n$(machine_name)$(directory_name)$(git_dirty)%k%f\nzsh› '
-set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
+next_segment() {
+    local NEXT_FG=$1
+    local NEXT_BG=$2
+
+    echo -n "$(color $SEGMENT_BG $NEXT_BG)$symbols[arrow]$(color_reset)"
+
+    SEGMENT_FG=$NEXT_FG
+    SEGMENT_BG=$NEXT_BG
 }
 
-precmd() {
-  title "zsh" "%m" "%55<...<%~"
-  set_prompt
+segment_time() {
+    write_segment "$symbols[clock] $(date +"%H:%M")"
 }
+
+segment_pwd() {
+    next_segment black yellow
+    write_segment "$symbols[folder] %~"
+}
+
+segment_hostname() {
+    next_segment black cyan
+    write_segment "$symbols[os] $(hostname)"
+}
+
+segment_dotnet() {
+    if type dotnet >/dev/null 2>/dev/null; then
+        next_segment white blue
+        write_segment "$symbols[dotnet] $(dotnet --version)"
+    fi
+}
+
+write_prompt() {
+    segment_time
+    segment_hostname
+    segment_pwd
+    segment_dotnet
+
+    echo "$(color_reset)"
+    echo "$symbols[prompt] "
+}
+
+export PROMPT="$(write_prompt)"
