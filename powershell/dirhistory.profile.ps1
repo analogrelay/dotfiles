12c2974acb053fb966e5f17ef065b65d9b70337f
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-08-04 11:35:57 -0700

    fix some derps

--- powershell/dirhistory.profile.ps1
+++ powershell/dirhistory.profile.ps1
@@ -1,5 +1,6 @@
 Set-Alias cd Push-Location -Option AllScope
-Set-Alias bd Push-Location -Option AllScope
+Set-Alias bd Pop-Location -Option AllScope
+Set-Alias cdi Set-Location -Option AllScope
 
 function global:hd() {
     Get-Location -Stack
