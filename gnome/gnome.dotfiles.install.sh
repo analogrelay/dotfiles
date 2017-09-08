e7652795fd713d90079f3e6c53ffdeeeda74727e
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2017-09-08 09:40:21 -0700

    change wsl behavior to share dotfiles with Windows

--- gnome/gnome.dotfiles.install.sh
+++ gnome/gnome.dotfiles.install.sh
@@ -1,4 +1,6 @@
-for file in $(find "$DOTFILES_ROOT/gnome" -name *.dconf); do
-    info "Installing dconf setting $file"
-    dconf load / < $file
-done
+if type -p dconf >/dev/null 2>&1; then 
+    for file in $(find "$DOTFILES_ROOT/gnome" -name *.dconf); do
+        info "Installing dconf setting $file"
+        dconf load / < $file
+    done
+fi
