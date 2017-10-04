e126af7836f0db56609d1f4d69a789dbee63b828
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2017-10-04 10:22:11 -0700

    add PATH variable management

--- powershell/go.profile.ps1
+++
@@ -1,8 +0,0 @@
-# Set up GOPATH
-if(Test-Path Code:\) {
-    if(!(Test-Path Code:\Go)) {
-        mkdir Code:\Go | Out-Null
-    }
-    $env:GOPATH = (Convert-Path Code:\Go)
-    $env:PATH="$env:GOPATH\bin;$env:PATH"
-}
--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -2,39 +2,31 @@ $Global:DotFilesRoot = Convert-Path (Split-Path -Parent $PSScriptRoot)
 
 # Locate code root
 $DefaultCodeRoot = Join-Path $env:USERPROFILE "Code"
-if($env:PROJECTS) {
+if ($env:PROJECTS) {
     $CodeRoot = $env:PROJECTS
-} elseif(Test-Path $DefaultCodeRoot) {
+}
+elseif (Test-Path $DefaultCodeRoot) {
     $CodeRoot = $DefaultCodeRoot
     $env:PROJECTS = $CodeRoot
 }
 
-$DotNetPath = Join-Path "$env:USERPROFILE" ".dotnet\x64"
-$env:PATH="$DotNetPath;$env:PATH"
-
-# Put dotfiles bin on the path. Also put functions on the path since unlike ZSH, PowerShell
-# runs scripts in the same shell instance (so there's no need to load the files as though they
-# were functions
-$env:PATH="$env:ProgramFiles\Perforce;$DotFilesRoot\functions;$DotFilesRoot\bin;$env:PATH"
-
 # Add the modules from dotfiles to the module path
-$env:PSModulePath="$DotFilesRoot\powershell\modules;$env:PSModulePath"
+$env:PSModulePath = "$DotFilesRoot\powershell\modules;$env:PSModulePath"
 
 # Install (if needed) and import Modules
 $Modules = @(
     "posh-git")
 $Modules | ForEach-Object {
-    if(!(Get-Module -ListAvailable $_)) {
+    if (!(Get-Module -ListAvailable $_)) {
         Write-Host "Installing Module $_ ..."
         Install-Module $_ -Scope CurrentUser
     }
     Import-Module $_
 }
 
-
 # Load all modules, unless they are disabled
 dir "$DotFilesRoot\powershell\modules" | Where-Object { $_.PSIsContainer } | ForEach-Object {
-    if(!(Test-Path "$DotFilesRoot\powershell\modules\$($_.Name).disabled")) {
+    if (!(Test-Path "$DotFilesRoot\powershell\modules\$($_.Name).disabled")) {
         Import-Module ($_.Name)
     }
 }
@@ -45,10 +37,10 @@ function gitconfig! {
 
 function TwoLevelRecursiveDir($filter) {
     dir $DotFilesRoot | ForEach-Object {
-        if($_.Name -like $filter) {
+        if ($_.Name -like $filter) {
             $_
         }
-        if($_.PSIsContainer) {
+        if ($_.PSIsContainer) {
             dir $_.FullName -filter $filter
         }
     }
@@ -69,4 +61,5 @@ TwoLevelRecursiveDir "*.profile.ps1" | foreach {
 
 try {
     [Console]::CursorSize = 25
-} catch {}
+}
+catch {}
