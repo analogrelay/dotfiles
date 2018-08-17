03392d1c47600928bd9b6011793400f1e5500c6d
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2018-08-17 11:44:13 -0700

    Update clone script for VSTS

--- functions/clone.ps1
+++ functions/clone.ps1
@@ -1,17 +1,33 @@
 param([string]$Repo)
 
-$splat = $Repo.Split("/")
-if ($splat.Length -ne 2) {
-    throw "Invalid Repo Name: $Repo"
+if($Repo.StartsWith("http://") -or $Repo.StartsWith("https://")) {
+    throw "This script can only be used with 'ssh://' URLs or GitHub owner/repo references"
 }
 
-$owner = $splat[0]
-$repoName = $splat[1]
-
-if(!(Test-Path "Code:\$owner")) {
-    mkdir "Code:\$owner"
+if(!$Repo.StartsWith("ssh://")) {
+    $Repo = "ssh://git@github.com/$Repo"
 }
 
-pushd "Code:\$owner"
-git clone "git@github.com:$owner/$repoName"
+$RepoUri = [Uri]$Repo;
+
+# Identify the source
+if($RepoUri.Host.EndsWith(".visualstudio.com")) {
+    # Parse the owner and repo name:
+    $Owner = "$($RepoUri.UserInfo).visualstudio.com/$($RepoUri.Segments[1].TrimEnd("/"))"
+    $RepoName = $RepoUri.Segments[-1]
+} elseif(($RepoUri.Host -eq "github.com") -or ($RepoUri.Host -eq "www.github.com")) {
+    $Owner = $RepoUri.Segments[1].TrimEnd("/")
+    $RepoName = $RepoUri.Segments[2].TrimEnd("/")
+} else {
+    throw "Unknown repository URL. Use standard 'git clone' for this"
+}
+
+$Container = Join-Path Code:\ $Owner
+
+if(!(Test-Path $Container)) {
+    mkdir $Container | Out-Null
+}
+
+pushd $Container
+git clone $Repo $RepoName
 popd
