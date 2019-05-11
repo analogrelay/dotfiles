fb6ee43ff9a18b004970dc5980b721590b0cb2d0
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2019-05-11 09:26:37 -0700

    enable ssh-agent by default

--- zsh/zshrc.sh
+++ zsh/zshrc.sh
@@ -65,7 +65,7 @@ ZSH_THEME="vibrantcode"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
-plugins=(git)
+plugins=(git ssh-agent)
 
 source $ZSH/oh-my-zsh.sh
 
