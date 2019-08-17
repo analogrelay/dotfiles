0d8c23a17410bbb1eef80ba14da9babf647bbcfa
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2019-08-16 20:08:09 -0700

    fix up git install sh

--- git/install.sh
+++ git/install.sh
@@ -11,17 +11,15 @@ echo "Configuring Git..."
 read "? - What is your Git author name? " AUTHOR_NAME
 read "? - What is your Git author email? " AUTHOR_EMAIL
 
-echo "[user]" > ./git/gitauthor.config
-echo "    name = $AUTHOR_NAME" >> ./git/gitauthor.config
-echo "    email = $AUTHOR_EMAIL" >> ./git/gitauthor.config
-
-# Symlink the git config in place
 if [ -f ~/.gitconfig ]; then
     if confirm "A git config file already exists in '~/.gitconfig'. Remove it?"; then
         rm ~/.gitconfig
     fi
 fi
 
-if [ ! -f ~/.gitconfig ]; then
-    ln -s ~/.dotfiles/git/.gitconfig ~/.gitconfig
-fi
+# Copy the git config in
+cp ~/.dotfiles/git/.gitconfig ~/.gitconfig
+
+echo "[user]" >> ~/.gitconfig
+echo "    name = $AUTHOR_NAME" >> ~/.gitconfig
+echo "    email = $AUTHOR_EMAIL" >> ~/.gitconfig
