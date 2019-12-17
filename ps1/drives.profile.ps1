e330f983e2248eff7a162b01d23d61b4bf6d5d7b
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2019-12-17 12:31:12 -0800

    fix up code drive and vscode use Delugia in terminal

--- ps1/drives.profile.ps1
+++ ps1/drives.profile.ps1
@@ -3,18 +3,19 @@ if ($DotFilesRoot -and (!(Test-Path DotFiles:\))) {
     New-PSDrive -Name DotFiles -Root $DotFilesRoot -PSProvider FileSystem | Out-Null
 }
 
-if($PSVersionTable.Platform -eq "Win32NT") {
+if ($PSVersionTable.Platform -eq "Win32NT") {
     $CodeDir = "C:\Code"
     $ProfileCodeDir = Join-Path $env:USERPROFILE "Code"
-    if (Test-Path $ProfileCodeDir) {
+    if (!(Test-Path $CodeDir) -and (Test-Path $ProfileCodeDir)) {
         Write-Warning "Code directory is in $ProfileCodeDir. This is deprecated as it has long path problems on Windows."
         $CodeDir = $ProfileCodeDir
     }
-} else {
+}
+else {
     $CodeDir = "$env:HOME/code"
 }
 
-if(!(Test-Path Code:\)) {
+if (!(Test-Path Code:\)) {
     if (!(Test-Path $CodeDir)) {
         Write-Host "Creating code directory: $CodeDir"
         New-Item -Type Directory $CodeDir | Out-Null
