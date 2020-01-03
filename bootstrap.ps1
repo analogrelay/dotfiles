32f949cee63c96e1f509498a5fdf051435e51a2f
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2020-01-03 10:12:12 -0800

    add initial bootstrap script

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,2 +1,29 @@
 # Bootstraps the installation of the dotfiles scripts from scratch
-throw "TODO"
+
+function Test-Command($CommandName) {
+    # Calling Get-Command, even with ErrorAction SilentlyContinue, spams
+    # $error with errors. This avoids that.
+    !!(Get-Command "$CommandName*" | 
+        Where-Object {
+            [System.IO.Path]::GetFileNameWithoutExtension($_.Name) -eq $CommandName
+        })
+}
+
+# Ensure scoop and git are installed
+if (!(Test-Command "scoop")) {
+    Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+}
+
+if (!(Test-Command "git")) {
+    scoop install git
+}
+
+# First, check if it's already cloned
+$DotFilesRoot = Join-Path ([Environment]::GetFolderPath("UserProfile")) ".dotfiles"
+if (!(Test-Path $DotFilesRoot)) {
+    # It doesn't exist, clone it
+    git clone "https://github.com/anurse/dotfiles" $DotFilesRoot
+}
+
+# Check if we're running out of the repo
+Write-Host "Script Root: $PSScriptRoot"
