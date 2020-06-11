edbf1a7f17fa045d1a1917e749bddac987cdf95e
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2020-06-10 21:57:11 -0700

    more gpg things

---
+++ zsh/zshrc.d/gpg.sh
@@ -0,0 +1,5 @@
+if has gpg-agent && [ -f /usr/local/bin/pinentry ]; then
+    echo "Launching gpg-agent"
+    killall gpg-agent
+    gpg-agent --daemon --pinentry-program /usr/local/bin/pinentry
+fi
