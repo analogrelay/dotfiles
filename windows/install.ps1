7291fe96a85969cc288fd1b0dbb7156dfffaf54e
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2020-07-13 11:22:14 -0700

    debug fix

--- windows/install.ps1
+++ windows/install.ps1
@@ -1,51 +1,41 @@
 param(
-    [Parameter(Mandatory = $false)][switch]$WhatIf,
-    [Parameter(Mandatory = $false)][switch]$Debug
+    [Parameter(Mandatory = $false)][switch]$WhatIf
 )
 
 if(!$IsWindows -and !$WhatIf) {
     throw "Cannot actually install components on non-Windows platforms."
 }
 
-$oldDebugPref = $DebugPreference
-try {
-    if($Debug) {
-        $DebugPreference = "Continue"
+Write-Debug "Loading scoopfile..."
+$Definitions = & "$PSScriptRoot/Get-Scoopfile.ps1"
+
+Write-Debug "Running items..."
+$Definitions | ForEach-Object {
+    $name = $_.Name
+    Write-Debug "Processing $name"
+    $isInstalled = $false
+    if ($_.Check -ne $null) {
+        Write-Debug "Running checker for $name ..."
+        $isInstalled = & $_.Check $_
+    } else {
+        Write-Debug "No checker for $name!"
     }
 
-    Write-Debug "Loading scoopfile..."
-    $Definitions = & "$PSScriptRoot/Get-Scoopfile.ps1"
-
-    Write-Debug "Running items..."
-    $Definitions | ForEach-Object {
-        $name = $_.Name
-        Write-Debug "Processing $name"
-        $isInstalled = $false
-        if ($_.Check -ne $null) {
-            Write-Debug "Running checker for $name ..."
-            $isInstalled = & $_.Check $_
-        } else {
-            Write-Debug "No checker for $name!"
+    if ($isInstalled) {
+        Write-Host "Using $name"
+    }
+    else {
+        Write-Host -ForegroundColor Green "Installing $name ..."
+        if ($_.Install -eq $null) {
+            Write-Error "No install command defined for $name!"
         }
-
-        if ($isInstalled) {
-            Write-Host "Using $name"
-        }
-        else {
-            Write-Host -ForegroundColor Green "Installing $name ..."
-            if ($_.Install -eq $null) {
-                Write-Error "No install command defined for $name!"
-            }
-            elseif (!$WhatIf) {
-                try {
-                    Write-Debug "Running installer for $name ..."
-                    & $_.Install $_
-                } catch {
-                    Write-Error "Installing $name failed with $($error[0])"
-                }
+        elseif (!$WhatIf) {
+            try {
+                Write-Debug "Running installer for $name ..."
+                & $_.Install $_
+            } catch {
+                Write-Error "Installing $name failed with $($error[0])"
             }
         }
     }
-} finally {
-    $DebugPreference = $oldDebugPref
 }
