0afbc160c8260d1a1c367d5614b5c8e8dcc505b2
Andrew Stanton-Nurse <andrew@stanton-nurse.com>
2020-07-21 14:12:22 -0700

    fix vscode linking on Windows

--- vscode/install.ps1
+++ vscode/install.ps1
@@ -3,22 +3,16 @@ if (!$DotFilesInstalling) { throw "This script should only be run during dotfile
 # Install to both Code and Code - Insiders
 $CodeVersions = @("Code", "Code - Insiders")
 
-$CodeDotfilesRoot = Join-Path $DotFilesRoot "vscode"
-$DotFilesSettings = Join-Path $CodeDotfilesRoot "settings.json"
-$DotFilesKeybinds = Join-Path $CodeDotfilesRoot "keybindings.json"
+$CodeDotfilesRoot = Join-Path (Join-Path $DotFilesRoot "vscode") "user"
 
 $CodeVersions | ForEach-Object {
-    Write-Host "Installing Settings symlinks for $_"
+    Write-Host "Installing Settings junction for $_"
     $UserSettingsRoot = Join-Path (Join-Path $env:APPDATA $_) "User"
 
-    if (!(Test-Path $UserSettingsRoot)) {
-        New-Item -Type Directory $UserSettingsRoot | Out-Null
+    if (Test-Path $UserSettingsRoot) {
+        Remove-Item -Recurse -Force $UserSettingsRoot
     }
 
-    $SettingsFile = Join-Path $UserSettingsRoot "settings.json"
-    $KeybindingsFile = Join-Path $UserSettingsRoot "keybindings.json"
-
-    # Create Symlinks
-    New-Link -Target $DotFilesSettings -Destination $SettingsFile
-    New-Link -Target $DotFilesKeybinds -Destination $KeybindingsFile
+    # Create Junction
+    New-Item -Value $CodeDotfilesRoot -Path $UserSettingsRoot -ItemType SymbolicLink | Out-Null
 }
