851e2b527f7a81ce41d4e60a19f1896f70695372
Andrew Stanton-Nurse <git@analogrelay.net>
2021-05-28 14:58:41 -0700

    remove install.sh to prevent auto install on Codespaces

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# "Alias" for ./script/setup, since some automated tools require a ./install.sh
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-exec "$DIR/script/setup"
