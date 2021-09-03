3ce0a696acf682b4599ade720e5d4faf6fb9e313
Andrew Stanton-Nurse <anurse@github.com>
2021-09-03 20:40:33 +0000

    updates to gpg signature handling

--- modules/zsh/zshrc.d/gpg.sh
+++ modules/zsh/zshrc.d/gpg.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-git config --file ~/.gitlocal user.signingkey "42ECAC889F98413E5C23970604277A6059CB213C"
-git config --file ~/.gitlocal commit.gpgsign true
+keyid="42ECAC889F98413E5C23970604277A6059CB213C"
+
+if [ "$CODESPACES" = "true" ] || gpg >/dev/null 2>&1; then
+    git config --file ~/.gitlocal commit.gpgsign true
+
+    # Use default signing key if present
+    if gpg --list-secret-keys "$keyid"; then
+        git config --file ~/.gitlocal user.signingkey "$keyid"
+    fi
+fi
